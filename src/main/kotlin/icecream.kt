1693bba106558fae95570b195dad0ba78da76766
acepele <acepele@students.mchenry.edu>
2022-09-02 23:33:58 -0500

    Initial commit

---
+++ src/main/kotlin/icecream.kt
@@ -0,0 +1,8 @@
+fun main() {
+    print("This is a list of some of the supplies needed for a coffee shop:\n")
+    val coffeeShop = arrayOf("1-Espresso machine ", "2-Coffee grinder ", "3-Industrial blenders ", "4-Oven and toasters ", "5-Automatic drip coffee makers ", "6-Curbside pickup and supplies ")
+
+    for (element in coffeeShop) {
+        println(element)
+    }
+}
